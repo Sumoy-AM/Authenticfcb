769cc70e2516d6e1f5e9bf5f06c594a86427185b
Sumoy <sumoyalvaradom2019@gmail.com>
2025-02-16 22:19:33 -0500

    Initial commit

---
+++ app/src/main/java/com/example/authenticfcb/Register.java
@@ -0,0 +1,73 @@
+package com.example.authenticfcb;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class Register extends AppCompatActivity {
+    private EditText email;
+    private EditText password;
+    private Button Register;
+    private FirebaseAuth auth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_register);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        email=findViewById(R.id.email);
+        password=findViewById(R.id.password);
+        Register=findViewById(R.id.Register);
+        auth=FirebaseAuth.getInstance();
+
+        Register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String txt_email = email.getText().toString();
+                String txt_password = password.getText().toString();
+
+                if (TextUtils.isEmpty(txt_email) || TextUtils.isEmpty(txt_password)) {
+                    Toast.makeText(Register.this, "Empy Credencials", Toast.LENGTH_SHORT).show();
+                }
+                else if (txt_password.length() < 6) {
+                    Toast.makeText(Register.this, "Password too short", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    registerUser(txt_email, txt_password);
+                }
+            }
+        });
+            return insets;
+        });
+    }
+
+    private void registerUser(String Email, String Password) {
+        auth.createUserWithEmailAndPassword(Email, Password).addOnCompleteListener(Register.this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+            if (task.isSuccessful()){
+                Toast.makeText(Register.this, "Register is Successful", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(Register.this, "Register Failed", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
